6299c890d8f542ae329d5f2e3b2d63aa73cb0b3a
ptamayo <ptamayo97@yahoo.com>
2019-12-21 17:46:52 -0800

    card

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,7 +2,42 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-export const IndexPageTemplate = ({}) => <Fragment></Fragment>;
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+
+export const IndexPageTemplate = ({}) => (
+  <Fragment>
+    <div>
+      <section className="hero is-info is-large">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns ">
+              <div className="column is-half">
+                <h1 className="title">Large title</h1>
+                <h2 className="subtitle">Large subtitle</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <Card />
+            </div>
+            <div className="column">
+              <Card />
+            </div>
+            <div className="column">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </Fragment>
+);
 
 IndexPageTemplate.propTypes = {};
 
