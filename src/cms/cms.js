34b448a5b8b0a077e890713eea7e5e56e87f6899
ptamayo <ptamayo97@yahoo.com>
2020-01-26 20:00:40 -0800

    added tapes page import in cms file

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,9 +4,11 @@ import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import TapesPagePreview from "./preview-templates/TapesPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("home", IndexPagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("tapes", TapesPagePreview);
